5929379f599c33ee01d522be4fd7a69f10105927
angel <angeldavidbetancourtvera@gmail.com>
2026-06-27 17:39:36 -0400

    update is done

--- base_project/settings.py
+++ base_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'newspapers',
     
+    
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'base_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- newspapers/views.py
+++ newspapers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import UpdateView
 
 # Create your views here.
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'update.html'
