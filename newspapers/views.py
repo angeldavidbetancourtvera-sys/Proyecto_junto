fd414eaec65f309ac859cb5dc61af94044498c6d
jesus <jesusenrique13721@gmail.com>
2026-06-27 18:55:30 -0400

    detail is done

--- newspapers/views.py
+++ newspapers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, UpdateView
+from django.views.generic import ListView, UpdateView, DetailView
 
 class PostListView(ListView):
     model = Post
@@ -10,3 +10,7 @@ class PostUpdateView(UpdateView):
     model = Post
     fields = ['title', 'content']
     template_name = 'update.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
